88a4852e50545fff2750035db5c706f3d3669b18
tranthithanhngan <5951071061@st.utc2.edu.vn>
2021-05-04 15:06:34 +0700

    first commit

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+using System.Data;
+
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins:"http://mywebclient.azurewebsites.net",headers:"*",methods:"*")]
+    public class StudentController : ApiController
+    {
+
+        private SqlConnection _conn;
+        private SqlDataAdapter _adapter;
+
+
+        // GET api/<controller>
+        public IEnumerable<Student> Get()
+        {
+            _conn = new SqlConnection("Data Source=DESKTOP-EDKIR8O;Initial Catalog=Nawab;Integrated Security=True ");
+            DataTable _dt = new DataTable();
+            var query = "select * from student";
+            _adapter = new SqlDataAdapter 
+            { 
+                SelectCommand = new SqlCommand(query, _conn) 
+            };
+
+            _adapter.Fill(_dt);
+            List<Student> students = new List<Models.Student>(_dt.Rows.Count);
+            if(_dt.Rows.Count>0)
+            {
+                foreach(DataRow studentReocord in _dt.Rows)
+                {
+                    students.Add(new ReadStudent(studentReocord));
+                }    
+            }    
+            return students;
+        }
+
+        // GET api/<controller>/5
+        public IEnumerable<Student> Get(int id)
+        {
+
+            _conn = new SqlConnection("Data Source=DESKTOP-EDKIR8O;Initial Catalog=Nawab;Integrated Security=True ");
+            DataTable _dt = new DataTable();
+            var query = "select * from student where id = "+ id ;
+            _adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand(query, _conn)
+            };
+
+            _adapter.Fill(_dt);
+            List<Student> students = new List<Models.Student>(_dt.Rows.Count);
+            if (_dt.Rows.Count > 0)
+            {
+                foreach (DataRow studentReocord in _dt.Rows)
+                {
+                    students.Add(new ReadStudent(studentReocord));
+                }
+            }
+            return students;
+        }
+
+        // POST api/<controller>
+        public string Post([FromBody] CreateStudent value)
+        {
+            _conn = new SqlConnection("Data Source=DESKTOP-EDKIR8O;Initial Catalog=Nawab;Integrated Security=True ");
+
+            var query = "insert into student (f_name, m_name, l_name, address, birthDate, score)    values (@f_name, @m_name, @l_name, @address, @birthDate, @score)";
+            SqlCommand insertCommand = new SqlCommand(query, _conn);
+            insertCommand.Parameters.AddWithValue("@f_name", value.f_name);
+            insertCommand.Parameters.AddWithValue("@m_name", value.m_name);
+
+            insertCommand.Parameters.AddWithValue("@l_name" , value.l_name);
+
+            insertCommand.Parameters.AddWithValue("@address", value.address);
+
+            insertCommand.Parameters.AddWithValue("@birthDate", value.birthDate);
+
+            insertCommand.Parameters.AddWithValue("@score", value.score);
+
+            _conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if( result > 0 )
+            {
+               return "Them thanh cong";
+            }
+            else
+            {
+                return "Them that bai";
+            }
+        }
+
+        // PUT api/<controller>/5
+        public string Put(int id, [FromBody] CreateStudent value)
+        {
+            _conn = new SqlConnection("Data Source=DESKTOP-EDKIR8O;Initial Catalog=Nawab;Integrated Security=True ");
+
+            var query = "update student set f_name=@f_name, m_name=@m_name, l_name=@l_name, address=@ address, birthDate=@birthDate, score=@score where id = "+id;
+            SqlCommand insertCommand = new SqlCommand(query, _conn);
+            insertCommand.Parameters.AddWithValue("@f_name", value.f_name);
+            insertCommand.Parameters.AddWithValue("@m_name", value.m_name);
+
+            insertCommand.Parameters.AddWithValue("@l_name", value.l_name);
+
+            insertCommand.Parameters.AddWithValue("@address", value.address);
+
+            insertCommand.Parameters.AddWithValue("@birthDate", value.birthDate);
+
+            insertCommand.Parameters.AddWithValue("@score", value.score);
+
+            _conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return "Them thanh cong";
+            }
+            else
+            {
+                return "Them that bai";
+            }
+        }
+
+        // DELETE api/<controller>/5
+        public string Delete(int id)
+        {
+
+            _conn = new SqlConnection("Data Source=DESKTOP-EDKIR8O;Initial Catalog=Nawab;Integrated Security=True ");
+
+            var query = "delete from student  where id = " + id;
+            SqlCommand insertCommand = new SqlCommand(query, _conn);
+           
+            _conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return "Them thanh cong";
+            }
+            else
+            {
+                return "Them that bai";
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string f_name { get; set; }
+        public string m_name { get; set; }
+        public string l_name { get; set; }
+        public string address { get; set; }
+        public string birthDate { get; set; }
+        public string score { get; set; }
+        public string dep_id { get; set; }
+
+    }
+
+    public class ReadStudent: Student
+    {
+        public ReadStudent(DataRow row)
+        {
+            Id = Convert.ToInt32(row["Id"]);
+            f_name = row["f_name"].ToString();
+            m_name = row["m_name"].ToString();
+            l_name = row["l_name"].ToString();
+            address = row["address"].ToString();
+            birthDate = row["birthDate"].ToString();
+            score = row["score"].ToString();
+           
+        }
+    }
+
+    public class CreateStudent: Student
+    {
+
+    }
+}
